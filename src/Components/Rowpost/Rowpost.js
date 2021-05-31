809164ddb6b20fa267ff10edb46f4be75f677073
Jithin Joshi <jithinjoshi2000@gmail.com>
2021-05-31 15:56:32 +0530

    youtube video provided,netflix modified

--- src/Components/Rowpost/Rowpost.js
+++ src/Components/Rowpost/Rowpost.js
@@ -1,12 +1,15 @@
 import React,{useEffect,useState} from 'react'
 import './Rowpost.css'
 import axios  from '../../axios'
-import {imageUrl} from '../../Constants/constants'
+import {imageUrl,API_KEY} from '../../Constants/constants'
+import YouTube from 'react-youtube'
+
 
 
 
 function Rowpost(props) {
     const [movie, setMovie] = useState([])
+    const [urls,setUrls]=useState()
     useEffect(() => {
         axios.get(props.urls).then((response)=>{
             console.log(response.data);
@@ -15,6 +18,25 @@ function Rowpost(props) {
 
        
     }, [])
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          
+          autoplay: 1,
+        },
+      };
+      const handleMovie=(id)=>{
+          console.log(id)
+          axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
+              if(response.data.results.length!==0){
+                setUrls(response.data.results[0])
+              }
+              else{
+                  console.log("Array Empty")
+              }
+          })
+      }
     return (
         <div className="row">
             <h2>{props.title}</h2>
@@ -22,12 +44,14 @@ function Rowpost(props) {
               {
                   movie.map((obj)=>{
                       return(
-                        <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+                        <img onClick={()=>handleMovie(obj.id)}  className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
                       )
                   })
               }
                 
             </div>
+            
+   {   urls  &&  <YouTube videoId={urls.key} opts={opts} />}
 
             
         </div>
